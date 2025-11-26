fb1a3230ec3342fe14ad0623d78db39ac8226f81
Kzsanjeet <sanjeetkazithapa@gmail.com>
2025-11-26 11:20:49 +0545

    changed for render

--- index.js
+++ index.js
@@ -148,30 +148,12 @@ const PORT = process.env.PORT || 5000;
 // check()
 
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 // Export for Vercel
 // module.exports = app;
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);
 
  // "node": "18.x"
-
-
- //vercel json
- // {
-//   "version": 2,
-//   "builds": [
-//     {
-//       "src": "./index.js",
-//       "use": "@vercel/node"
-//     }
-//   ],
-//   "routes": [
-//     {
-//       "src": "/(.*)",
-//       "dest": "/"
-//     }
-//   ]
-// }
